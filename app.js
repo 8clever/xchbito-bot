f27302a67ae038d75bd7bf17858c3cf83ef880e6
8clever <godofluck89@gmail.com>
2017-05-05 01:35:50 +0300

    small fix

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
-const auth = require("./auth");
-const safe = require("safe");
-const cb = function(err) {
-	if (err)
-		console.error(err);
-};
+const TwitchBot = require('node-twitchbot');
 
-auth(safe.sure(cb, token => {
-	console.log(token);
-}));
+const Bot = new TwitchBot({
+	username : 'xchbito',
+	oauth    : 'oauth:lyrfpe260onmdi74rvpwh0krmiqaa5',
+	channel  : 'hikka_live'
+});
+
+Bot.connect().then(() => {
+	Bot.listen((err, chatter) => {
+		if(err)
+			return console.log(err);
+
+		let message = chatter.msg;
+
+		if(/!пошути_про_маму/.test(message))
+			Bot.msg('шутка про маму)))');
+
+		if(/!joke @([\D\d]*)/.test(message)) {
+			let match = message.match(/!joke @([\D\d]*)/);
+
+			if (match && match[1])
+				Bot.msg(`Я пошутил над @${match[1]}`);
+		}
+
+		if (/!xchbitobot/.test(message))
+			Bot.msg("Hello");
+
+		console.log(message)
+	});
+
+	setTimeout(function() {
+		Bot.msg("Skynet returning from hell!!!");
+	}, 60000);
+
+	console.log("Bot runned");
+}).catch(err => {
+	console.log('Connection error!');
+	console.log(err)
+});

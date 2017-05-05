9a9b2e767a12406a2d2d396a8f7ab9dd1bf29a26
Ivan Vityaev <godofluck89@gmail.com>
2017-05-06 01:40:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -80,6 +80,8 @@ Bot.connect().then(() => {
 		saveJoke.call(Bot, chatter.msg);
 		getJokeList.call(Bot, chatter.msg, chatter.level);
 		rmJoke.call(Bot, chatter.msg, chatter.level);
+		
+		console.log(chatter.level);
 	});
 
 	setInterval(function() {

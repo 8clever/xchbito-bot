8c401683c2617b378a389ed7ae71080f20086a7d
Ivan Vityaev <godofluck89@gmail.com>
2017-05-05 23:23:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,57 @@
 const TwitchBot = require('node-twitchbot');
-
+const BOT_NAME = "xchbitobot"
 const Bot = new TwitchBot({
-	username : 'xchbito',
+	username : BOT_NAME,
 	oauth    : 'oauth:lyrfpe260onmdi74rvpwh0krmiqaa5',
 	channel  : 'hikka_live'
 });
 
+function jokeForMom (msg) {
+	if(!/!пошути_про_маму/.test(msg)) return;
+	this.msg('шутка про маму)))');
+}
+
+function jokeTo (msg) {
+	if(!/!joke @([\D\d]*)/.test(msg)) return;
+	
+	let match = msg.match(/!joke @([\D\d]*)/);
+
+	if (!(match && match[1])) return;
+	this.msg(`Я пошутил над @${match[1]}`);
+}
+
+function sayHello (msg) {
+	if (!/!xchbitobot/.test(msg)) return;
+	this.msg("Hello");
+}
+
+function saveJoke (msg) {
+	if (!/!joke !push/.test(msg)) return;
+	
+	let match = msg.match(/!joke !push ([\d\D]*)/);
+	if (!(match && match[1])) return;
+}
+
 Bot.connect().then(() => {
+	var countMsg = 0;
+	
 	Bot.listen((err, chatter) => {
-		if(err)
-			return console.log(err);
+		if(err) return console.log(err);
 
-		let message = chatter.msg;
-
-		if(/!пошути_про_маму/.test(message))
-			Bot.msg('шутка про маму)))');
-
-		if(/!joke @([\D\d]*)/.test(message)) {
-			let match = message.match(/!joke @([\D\d]*)/);
-
-			if (match && match[1])
-				Bot.msg(`Я пошутил над @${match[1]}`);
-		}
-
-		if (/!xchbitobot/.test(message))
-			Bot.msg("Hello");
-
-		console.log(message)
+		if (chatter.user != BOT_NAME)
+			countMsg++;
+		
+		jokeForMom.call(Bot, chatter.msg);
+		jokeTo.call(Bot, chatter.msg);
+		sayHello.call(Bot, chatter.msg);
+		saveJoke.call(Bot, chatter.msg);
 	});
 
 	setInterval(function() {
-		Bot.msg("Skynet returning from hell!!!");
+		if (countMsg > 5) {
+			Bot.msg("Skynet returning from hell!!!");
+			countMsg = 0;
+		}
 	}, 60000 * 10);
 
 	console.log("Bot runned");

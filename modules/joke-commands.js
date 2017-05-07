b3a0d236a7697195fd801699d4c58ae7438c7884
Ivan Vityaev <godofluck89@gmail.com>
2017-05-07 17:10:43 +0300

    Update joke-commands.js

--- modules/joke-commands.js
+++ modules/joke-commands.js
@@ -1,9 +1,22 @@
 const _ = require("lodash");
+const safe = require("safe");
 
-module.exports = function({ collections }) {
-	return {
+module.exports = function({ collections, options }, Bot) {
+	let getRandomUser = function({ channel }, cb) {
+		Bot.api({
+		    url: `https://tmi.twitch.tv/group/user/${channel}/chatters`,
+		    headers: {
+		    	"Client-ID": options.clientId
+		    }
+		}, safe.sure_result(cb, (res, body) => {
+			let users = _.union(body.chatters.moderators, body.chatters.viewers);
+			return users[_.random(0, users.length - 1)];
+		}));
+	}
+		
+	let fns = {
 
-		jokeTo: function ({ msg }) {
+		jokeTo: function ({ msg, channel }) {
 			if(!/!joke @([\D\d]*)/.test(msg)) return;
 
 			let match = msg.match(/!joke @([\D\d]*)/);
@@ -17,12 +30,21 @@ module.exports = function({ collections }) {
 			let name = "@" + match[1];
 			let string = joke.id;
 			let regex = /<name>/;
+			let regexRandom = /<rname>/
 
 			if (regex.test(string))
 				string = string.replace(regex, name);
 			else
 				string = `${string} ${name}`;
-
+			
+			if (regexRandom.test(string))
+				return getRandomUser({ channel }, (err, randomName) => {
+					if (err) return;
+					
+					string = string.replace(regexRandom, `@${randomName}`);
+					this.msg(string);
+				})
+			
 			this.msg(string);
 		},
 
@@ -63,6 +85,8 @@ module.exports = function({ collections }) {
 
 			if (!collections.jokes.size()) this.msg("Jokes list is empty");
 		}
-	}
+	};
+	
+	return fns;
 };
 

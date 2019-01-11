a2d10336381667c9f487a927f018e254a727a228
ivanv <ivanv@pushok.com>
2019-01-11 19:21:26 +0300

    rewrite to classes

--- app.js
+++ app.js
@@ -1,60 +1,18 @@
-const tmi = require("tmi.js");
-const Collection = require('json-collections');
-const safe = require("safe");
 const _ = require("lodash");
-const cfg = Object.freeze({
-	color: "CadetBlue",
-	options: {
-		clientId: "aqh3wb4avo6r5ig0dvfl1n3c0vvi7r",
-		debug: true
-	},
-	connection: {
-		reconnect: true
-	},
-	identity: {
-		username: "xchbitobot",
-		password: "oauth:lyrfpe260onmdi74rvpwh0krmiqaa5"
-	},
-	channels: [
-		"hikka_live",
-		"8clever"
-	],
-	collections: {
-		jokes: Collection({name: 'jokes', filepath: 'jokes' })
-	},
-	bots: [
-		"chatdb",
-		"faegwent",
-		"nightbot",
-		"xchbitobot"
-	]
+
+const Jokes = require("./lib/Jokes");
+const Bot = require("./lib/Bot");
+
+let bot = new Bot({
+	clientId: "aqh3wb4avo6r5ig0dvfl1n3c0vvi7r",
+	username: "xchbitobot",
+	password: "oauth:lyrfpe260onmdi74rvpwh0krmiqaa5",
+	channel: "8clever"
 });
 
-tmi.client.prototype.getUsers = function(channel, cb) {
-	let hashRegex = /#/;
+bot.connect().then(() => {
+	bot.color("Red");
+	bot.addPlugin(Jokes);
 
-	if (hashRegex.test(channel))
-		channel = channel.replace(hashRegex, "");
-
-	this.api({
-		url: `https://tmi.twitch.tv/group/user/${channel}/chatters`,
-		headers: {
-			"Client-ID": this.getOptions().options.clientId
-		}
-	}, safe.sure_result(cb, (res, body) => {
-		let users = _.union(
-			_.get(body, "chatters.moderators", []),
-			_.get(body, "chatters.viewers", [])
-		);
-		users = _.reject(users, u => _.includes(this.getOptions().bots, u));
-		return users;
-	}));
-};
-
-const Bot = new tmi.client(cfg);
-Bot.connect().then(() => {
-	Bot.color(cfg.color);
-	require("./modules/bot")(Bot, cfg);
+	console.log("bot.connected!")
 });
-
-
